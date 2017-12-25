4293851a917900f16f18c683733c8a6bef984d85
fandytcc <fandytsui@gmail.com>
2017-12-25 10:43:26 +0100

    create artist overview page to see list of artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,15 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show]
 
   def index
     @artists = Artist.all
+  end
+
+  def show; end
+
+  private
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 
 end
