f56013527a6e5de88482e1a78a995ea7e5962b09
fandytcc <fandytsui@gmail.com>
2017-12-26 19:58:29 +0100

    change image sizes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :thumbnail do
     eager
-    resize_to_fit(400, 500)
+    resize_to_fit(400, 150)
     cloudinary_transformation :quality => 80
   end
 end
