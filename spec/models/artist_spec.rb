49437e539cd7fd60a93b8bed4a169cea14d635ea
fandytcc <fandytsui@gmail.com>
2017-12-26 19:36:53 +0100

    Add model specs: associations tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe Artist, type: :model do
       expect(Artist.order_by_name).to eq([artist2, artist3, artist1])
     end
   end
+
+  describe "association with song" do
+    let(:artist) { create :artist }
+    let!(:song) { create :song, artist: artist }
+
+    it "has many songs" do
+      song1 = artist.songs.new(title: "Title1")
+      song2 = artist.songs.new(title: "Title2")
+
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+    end
+
+    it "deletes associated songs" do
+      expect { artist.destroy }.to change(Song, :count).by(-1)
+    end
+  end
+
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Song, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:title) }
   end
+
+  describe "association with artist" do
+    it { is_expected.to belong_to :artist}
+  end
+
 end
