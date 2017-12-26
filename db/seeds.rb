20e12f3696c67e976af2dae5e7ceb859d8f301d6
fandytcc <fandytsui@gmail.com>
2017-12-26 12:02:51 +0100

    Configure Heroku & deploy apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,15 +53,15 @@ artists.sample(20).each do |artist|
 end
 
 #photos
-photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279984/john-legend_tud4rr.jpg", artist: artist_1)
-photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279866/celebrity-of-the-week-music-superstar-lady-gaga-hollywood-celebrities-movie-stars-beverly-hills-magazine-1_tqzkcg.jpg", artist: artist_2)
-photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279724/po63w2ye8k_rmnk5c.jpg", artist: artist_3 )
-
-photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279625/Gavin-James_urhae9.jpg", artist: artist_4 )
-photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279578/6ff5c34fae327b11f8b23a80b721de86_jakfdi.jpg", artist: artist_5)
-photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279560/justin-bieber-tumblr-wallpapers-desktop-13976182_xlkzj7.jpg", artist: artist_6 )
-
-photo7 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279548/Top-10-Most-Popular-Hollywood-Singer-in-2015-Bruno-Mars_wnzp3m.jpg", artist: artist_7)
-photo8 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279494/21b02479f5f3f3e982ae4f05c3e2fbab-1200-80_smtq82.jpg", artist: artist_8)
-photo9 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279426/adele-1_qak3yc.jpg", artist: artist_9)
-photo10 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279388/ed-sheeran-net-worth_ubmflt.jpg", artist: artist_10)
+# photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279984/john-legend_tud4rr.jpg", artist: artist_1)
+# photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279866/celebrity-of-the-week-music-superstar-lady-gaga-hollywood-celebrities-movie-stars-beverly-hills-magazine-1_tqzkcg.jpg", artist: artist_2)
+# photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279724/po63w2ye8k_rmnk5c.jpg", artist: artist_3 )
+#
+# photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279625/Gavin-James_urhae9.jpg", artist: artist_4 )
+# photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279578/6ff5c34fae327b11f8b23a80b721de86_jakfdi.jpg", artist: artist_5)
+# photo6 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279560/justin-bieber-tumblr-wallpapers-desktop-13976182_xlkzj7.jpg", artist: artist_6 )
+#
+# photo7 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279548/Top-10-Most-Popular-Hollywood-Singer-in-2015-Bruno-Mars_wnzp3m.jpg", artist: artist_7)
+# photo8 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279494/21b02479f5f3f3e982ae4f05c3e2fbab-1200-80_smtq82.jpg", artist: artist_8)
+# photo9 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279426/adele-1_qak3yc.jpg", artist: artist_9)
+# photo10 = Photo.create!(remote_image_url: "http://res.cloudinary.com/dp99fz03x/image/upload/v1514279388/ed-sheeran-net-worth_ubmflt.jpg", artist: artist_10)
