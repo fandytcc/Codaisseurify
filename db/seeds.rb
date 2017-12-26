5e2003264b13787ab6af3cdc802e21824bb267b4
fandytcc <fandytsui@gmail.com>
2017-12-26 17:04:22 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,25 +13,25 @@ User.destroy_all
 user = User.create!(email: "test@test.com", password: "123456")
 
 #artists
-artist_1 = Artist.create!(first_name: "John", last_name: "Legend", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_1 = Artist.create!(first_name: "John", last_name: "Legend", bio: "Grammy and Academy Award-winning singer-songwriter John Legend is known for such acclaimed albums as Get Lifted and Love in the Future.")
 
-artist_2 = Artist.create!(first_name: "Lady", last_name: "Gaga", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_2 = Artist.create!(first_name: "Lady", last_name: "Gaga", bio: "Pop icon Lady Gaga\'s debut album, The Fame, included the hits Just Dance and Poker Face. Her follow-up effort, The Fame Monster, was nominated for six Grammys.")
 
-artist_3 = Artist.create!(first_name: "Alicia", last_name: "Key", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_3 = Artist.create!(first_name: "Alicia", last_name: "Key", bio: "Alicia Keys is a multiple Grammy Award-winning singer-songwriter whose debut album, Songs in A Minor, went platinum five times over.")
 
-artist_4 = Artist.create!(first_name: "Gavin", last_name: "James", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_4 = Artist.create!(first_name: "Gavin", last_name: "James", bio: "Singer and songwriter who is known for his 2012 debut EP, Say Hello. The title track subsequently won the Song of the Year award from Ireland\'s Meteor Choice Music Prize.")
 
-artist_5 = Artist.create!(first_name: "Taylor", last_name: "Swift", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_5 = Artist.create!(first_name: "Taylor", last_name: "Swift", bio: "Grammy Award-winning singer/songwriter Taylor Swift made a splash in the country music world in 2006 and has gone on to become one of the top acts in popular music.")
 
-artist_6 = Artist.create!(first_name: "Justin", last_name: "Bieber", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_6 = Artist.create!(first_name: "Justin", last_name: "Bieber", bio: "Justin Bieber is a Grammy Award-winning Canadian pop star who was discovered via YouTube. His latest hits include Let Me Love You, Despacito (Remix), and I am the One.")
 
-artist_7 = Artist.create!(first_name: "Bruno", last_name: "Mars", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_7 = Artist.create!(first_name: "Bruno", last_name: "Mars", bio: "Bruno Mars is Grammy-winning singer/songwriter known for such hit songs as Nothin on You, Just the Way You Are, Locked Out of Heaven and ptown Funk.")
 
-artist_8 = Artist.create!(first_name: "Beyonce", last_name: "Morphs", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_8 = Artist.create!(first_name: "Beyonce", last_name: "Knowles", bio: "Beyoncé Knowles is a multi-platinum, Grammy Award-winning recording artist who's acclaimed for her thrilling vocals, videos and live shows.")
 
-artist_9 = Artist.create!(first_name: "Adele", last_name: "Laruie", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_9 = Artist.create!(first_name: "Adele", last_name: "Laruie", bio: "Singer Adele is a Grammy Award-winning artist whose throwback, soulful sound has made her an international success with hits like Rolling in the Deep, Someone Like You and Hello.")
 
-artist_10 = Artist.create!(first_name: "Ed", last_name: "Sheeran", bio: Faker::Hipster.paragraph[0..496] << "...")
+artist_10 = Artist.create!(first_name: "Ed", last_name: "Sheeran", bio: "U.K. artist Ed Sheeran is a Grammy-winning singer/songwriter known for hit songs like Thinking Out Loud and Photograph.")
 
 artists = [artist_1, artist_2, artist_3, artist_4, artist_5, artist_6, artist_7, artist_8, artist_9, artist_10]
 # 10.times do
